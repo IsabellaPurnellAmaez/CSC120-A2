64729f6a30fdce9849a396be86d340c87548e00e
Isabella Purnell-Amaez <ipurnellamaez@smith.edu>
2025-09-15 21:12:54 -0400

    workin on HW

--- computer.py
+++ computer.py
@@ -1,6 +1,6 @@
 class Computer:
 
-    # What attributes will it need: price, OS
+    # What attributes will it need?
     
     description: str
     processor_type: str
@@ -12,7 +12,29 @@ class Computer:
     
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def __init__(self, description, processorType, hardDriveCapacity, memory, OS, year, price):
+        self.description =  description
+        self.processor_type = processorType
+        self.hard_drive_capacity = hardDriveCapacity 
+        self.memory = memory
+        self.operating_system = OS
+        self.year_made = year
+        self.price = price
 
+       
+        
     # What methods will you need?
+    def printComputerSetUp(self):
+        print ({'description': self.description, 'processor_type': self.processor_type, 'hard_drive_capacity': self.hard_drive_capacity, 'memory': self.memory, 'operating_system': self.operating_system, 'year_made': self.year_made, 'price':self.price})
+    
+    def refurbish(self, OS:str):
+        self.operating_system = OS
+
+def main():
+    computer1: Computer = Computer('2018 MacBook Pro', 'Intel', 256, 16,'High Sierra', 2019, 1000)
+    computer1.printComputerSetUp()
+    computer1.refurbish("ah")
+    computer1.printComputerSetUp()
+
+if __name__ == "__main__":
+    main()
