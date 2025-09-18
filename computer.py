9861dda1c34f32f8c16dffd32550269d5b41213a
Isabella Purnell-Amaez <ipurnellamaez@smith.edu>
2025-09-17 22:16:14 -0400

    Completing Homework

--- computer.py
+++ computer.py
@@ -1,7 +1,5 @@
-class Computer:
-
-    # What attributes will it need?
-    
+class Computer:       
+    #list of attributes of a computer and their types         
     description: str
     processor_type: str
     hard_drive_capacity: int
@@ -10,9 +8,8 @@ class Computer:
     year_made: int
     price: int
     
-    # How will you set up your constructor?
-    # Remember: in python, all constructors have the same name (__init__)
-    def __init__(self, description, processorType, hardDriveCapacity, memory, OS, year, price):
+    def __init__(self, description, processorType, hardDriveCapacity, memory, OS, year, price): 
+        #setting up the computers
         self.description =  description
         self.processor_type = processorType
         self.hard_drive_capacity = hardDriveCapacity 
@@ -22,13 +19,22 @@ class Computer:
         self.price = price
 
        
-        
-    # What methods will you need?
     def printComputerSetUp(self):
-        print ({'description': self.description, 'processor_type': self.processor_type, 'hard_drive_capacity': self.hard_drive_capacity, 'memory': self.memory, 'operating_system': self.operating_system, 'year_made': self.year_made, 'price':self.price})
-        #prints out computer as a dictionary of all it's components. 
+        #prints out all the components of the specific computer in a way that's easy to read
+        #checks that the computer is set up correctly
+        print ('Description:', self.description)
+        print ('Processor Type:', self.processor_type)
+        print ('Hard Drive Capacity:', self.hard_drive_capacity)
+        print ('Memory:', self.memory)
+        print ('Operating System:', self.operating_system)
+        print ('Year Made:', self.year_made)
+        print ('Price:', self.price)
 
     def refurbish(self):
+        #update price based on age of machine
+
+        print ("Refurbishing", self.description)
+
         if self.year_made < 2000:
             self.price = 0 # too old to sell, donation only
         elif self.year_made < 2012:
@@ -38,14 +44,21 @@ class Computer:
         else:
             self.price = 1000 # recent stuff
     
+        print ("New price is", self.price)
+        print ("Done. \n")
+
+
     def updateOS(self, OS):
+        #updates the operating system
+        print ("Updating", self.description, "OS to", OS)
+        print("Updating inventory...")
         self.operating_system = OS
+        print("Done.\n")
+     
 
 def main():
-    computer1: Computer = Computer('2018 MacBook Pro', 'Intel', 256, 16,'High Sierra', 2017, 1000)
-    computer1.printComputerSetUp()
-    computer1.refurbish()
-    computer1.printComputerSetUp()
+    computer1: Computer = Computer('2018 MacBook Pro', 'Intel', 256, 16,'High Sierra', 2017, 1000)  
+    
 
 if __name__ == "__main__":
     main()
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -12,22 +12,55 @@ class ResaleShop:
         self.inventory = inventory 
     # What methods will you need?
 
-    
 
     def buy(self, computer:Computer):
+        print("Buying", computer.description)
+        print("Adding to inventory...")
         self.inventory.append(computer)
-        #1. call Computer(...) constructor #to create a new computer instance <== ASK ABOUT THIS STEP IN TUTORING!!
-        #2. call inventory.append(...) to add the new computer to the inventory
+        print("Done.\n")
 
+       
 
     def sell(self, computer:Computer):
         self.inventory.remove(computer)
 
     def print_inventory(self):
-        print(self.inventory)
+        for i in range (len(self.inventory)):
+            print ('Description:', self.inventory[i].description)
+            print ('Processor Type:', self.inventory[i].processor_type)
+            print ('Hard Drive Capacity:', self.inventory[i].hard_drive_capacity)
+            print ('Memory:', self.inventory[i].memory)
+            print ('Operating System:', self.inventory[i].operating_system)
+            print ('Year Made:', self.inventory[i].year_made)
+            print ('Price:', self.inventory[i].price)
+            print ('\n')
+
 
 
 def main():
+    print("-" * 21)
+    print("COMPUTER RESALE STORE")
+    print("-" * 21, '\n')
+    
+    Resale_Shop: ResaleShop = ResaleShop([])
+    
     computer1: Computer = Computer('2018 MacBook Pro', 'Intel', 256, 16,'High Sierra', 2017, 1000)  
+    computer2: Computer = Computer('Mac Pro (Late 2013)', '3.5 GHc 6-Core Intel Xeon E5', 1024, 64, 'MacOS Monterey', 2013, 550)  
+
+    Resale_Shop.buy(computer1)
+    Resale_Shop.buy(computer2)
+
+    Resale_Shop.print_inventory()
+
+    computer1.refurbish()
+
+    computer2.updateOS('High Sierra')
+
+    Resale_Shop.sell(computer1)
+
+    Resale_Shop.print_inventory()
+
+
+    
 if __name__ == "__main__":
     main()
